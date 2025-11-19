53b0e00f61108c498a5586bf34e24e07032d40fe
febrina1602 <112796775+febrina1602@users.noreply.github.com>
2025-11-19 22:50:09 +0700

    Adding some features

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,9 +23,9 @@ class DatabaseSeeder extends Seeder
             // DestinationCategorySeeder::class, 
             // DestinationSeeder::class,  
             MarketplaceSeeder::class,
-            OlehOlehSeeder::class,
-            PenginapanSeeder::class,
-            TransportSeeder::class
+            // OlehOlehSeeder::class,
+            // PenginapanSeeder::class,
+            // TransportSeeder::class
         ]);
     }
 }
